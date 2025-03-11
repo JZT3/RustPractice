3240bc4bd4c13f3e57cb2203dbc25820560d337c
Jervani Z Thompson <jzthompson20@gmail.com>
2025-03-11 14:28:06 -0700

    visualizing ownership principles

--- ownership.rs
+++ ownership.rs
@@ -1,4 +1,27 @@
 // Ownership Borrowing and References
+// Allows us to borrow temporarily borrow refernces to values 
+// without sacrificing memory safety.
 
 // Ownership
 // ----------
+// 1- Each value has an 'owner'
+// 2- There can only be one owner at a time
+// 3- When the owner goes out of scope, the value will be dropped.
+
+fn calc_length(s: &String) -> usize{
+  s.len()
+}
+
+fn main(){
+  // ----- Principle 1 -------
+  let string1 = String::from("RUST"); // Owner of this string
+  let len = calc_length(&string1); // Pass by reference
+  println!("Length of '{}' is {}.", string1, len);
+
+  // ----- Principle 2 ------
+  let string2 = string1;
+  // println!("{}", string1); <---- This will trigger error cause it no longer owns string
+  println!("{}", string2);
+}
+
+
