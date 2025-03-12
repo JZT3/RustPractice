77b21b65914d0e916981872bbe7f7e878e921bae
Jervani Z Thompson <jzthompson20@gmail.com>
2025-03-11 18:02:02 -0700

    added more basic examples

--- control_flow.rs
+++ control_flow.rs
@@ -16,4 +16,46 @@ fn main() {
   let condition = true;
   let number = if condition {5} else {6}; // Changing the 6 to "six" will throw a type error
   println!("Number: {number}");
+
+  // Loops
+  let mut counter = 0;
+  let result = loop {
+    counter += 1;
+
+    if counter == 10 {
+      break counter * 2;
+    }
+  };
+
+  println!("The result is {result}");
+
+  // Loop Lables
+  let rows = 3;
+  let cols = 3;
+  let mut matrix = vec![vec![0; cols]; rows];
+
+  'outer: for i in 0..rows {
+    'inner: for j in 0..cols {
+      matrix[i][j] = i * cols + j;
+      println!("matrix[{}][{}] = {}", i,j, matrix[i][j]);
+    }
+  }
+
+  println!("{:?}", matrix);
+
+
+  // While loop
+  let mut thing = 3;
+  while thing != 0 {
+    println!("{number}");
+    thing -= 1;
+  }
+  println!("BANG!!");
+
+  // Loop through collections
+  let a = [1,2,3,4,5];
+
+  for element in a {
+    println!("{element}")
+  }
 }
